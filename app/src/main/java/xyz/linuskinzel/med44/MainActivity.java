06e8f2bcbe400b3b9dfbb61cfca2c70417436ec7
fearthedear <linuskinzel@gmail.com>
2016-10-24 03:57:09 +0800

    setting up mainactivity

--- app/src/main/java/xyz/linuskinzel/med44/MainActivity.java
+++ app/src/main/java/xyz/linuskinzel/med44/MainActivity.java
@@ -1,13 +1,16 @@
 package xyz.linuskinzel.med44;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
