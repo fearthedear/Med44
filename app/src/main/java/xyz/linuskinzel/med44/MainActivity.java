76a2cf9a402a3cf2efb48e02277cd4a3c0b3363f
fearthedear <linuskinzel@gmail.com>
2016-10-26 19:28:51 +0800

    setting up initial setup screen. functional, but no design yet

--- app/src/main/java/xyz/linuskinzel/med44/MainActivity.java
+++ app/src/main/java/xyz/linuskinzel/med44/MainActivity.java
@@ -1,8 +1,12 @@
 package xyz.linuskinzel.med44;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +16,18 @@ public class MainActivity extends Activity {
         //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        //check if name is set in sharedpreferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        String name = prefs.getString("name", "no shared preference stored");
+
+        if (name == "no shared preference stored") {
+            Intent goToSetup = new Intent(this, setupActivity.class);
+            startActivity(goToSetup);
+        }
+        else {
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        }
     }
 }
